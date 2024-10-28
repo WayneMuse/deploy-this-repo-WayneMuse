f894fee362458d84ac86d360d0c23aeb18a02cbb
WayneMuse <wmuse@outlook.com>
2024-10-27 19:01:31 -0700

    ASDFGHJ,.

--- src/lib/server/database.js
+++ src/lib/server/database.js
@@ -2,8 +2,7 @@
 import postgres from 'postgres'
 
 // see env variables in .env
-import {PGCONNECT} from '$env/static/private';
+import { PGCONNECT } from '$env/dynamic/private';
 
-const sql = postgres(PGCONNECT, {} )
-
+const sql = postgres(PGCONNECT, {});
 export default sql;

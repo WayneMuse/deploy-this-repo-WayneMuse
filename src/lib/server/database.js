c4ee17250c9666f62c92a092bfffff67b35419a9
WayneMuse <wmuse@outlook.com>
2024-10-27 19:09:35 -0700

    Revert "ASDFGHJ,."
    This reverts commit f894fee362458d84ac86d360d0c23aeb18a02cbb.

--- src/lib/server/database.js
+++ src/lib/server/database.js
@@ -2,7 +2,8 @@
 import postgres from 'postgres'
 
 // see env variables in .env
-import { PGCONNECT } from '$env/dynamic/private';
+import {PGCONNECT} from '$env/static/private';
 
-const sql = postgres(PGCONNECT, {});
+const sql = postgres(PGCONNECT, {} )
+
 export default sql;
